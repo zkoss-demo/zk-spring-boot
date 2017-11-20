3f21ce4f17b13494c131e5b9b2f2c2e0a3b3d03d
Robert Wenzel <robertwenzel@potix.com>
2017-11-20 19:18:31 +0800

    cleanup gradle conf, + minor examples

--- src/main/java/zk/springboot/richlet/TestRichlet.java
+++ src/main/java/zk/springboot/richlet/TestRichlet.java
@@ -4,6 +4,8 @@ import org.zkoss.zk.ui.Executions;
 import org.zkoss.zk.ui.GenericRichlet;
 import org.zkoss.zk.ui.Page;
 import org.zkoss.zk.ui.Richlet;
+import org.zkoss.zk.ui.util.Clients;
+import org.zkoss.zul.Button;
 import org.zkoss.zul.Label;
 import org.zkoss.zul.Window;
 
@@ -13,5 +15,8 @@ public class TestRichlet extends GenericRichlet {
 	public void service(Page page) throws Exception {
 		Window window = new Window("ZK-Spring-Boot Richlet", "normal", true);
 		window.setPage(page);
+		Button button = new Button("test");
+		button.addEventListener("onClick", event -> Clients.showNotification("clicked"));
+		window.appendChild(button);
 	}
 }
