c9f169be008f727d69a95c904aa964a2fe851159
Robert Wenzel <robertwenzel@potix.com>
2017-12-01 19:46:00 +0800

    replace @Import with dedicated annotations

--- src/main/java/zk/springboot/Application.java
+++ src/main/java/zk/springboot/Application.java
@@ -2,14 +2,13 @@ package zk.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Import;
-import zk.springboot.config.ZKCEConfig;
-import zk.springboot.config.ZKEEConfig;
+//import zk.springboot.config.ZKCEApplication;
+import zk.springboot.config.ZKEEApplication;
 
 @SpringBootApplication
-//@Import(ZKCEConfig.class) /*ZK CE config only*/
-@Import(ZKEEConfig.class) /*ZK EE config includes CE*/
-public class Application  {
+//@ZKCEApplication
+@ZKEEApplication
+public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
