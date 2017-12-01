0ddac6ca874e11692fd15c1d02c2e6df961f76ba
Robert Wenzel <robertwenzel@potix.com>
2017-12-01 19:53:13 +0800

    add missing comment

--- src/main/java/zk/springboot/config/ZKEEApplication.java
+++ src/main/java/zk/springboot/config/ZKEEApplication.java
@@ -9,6 +9,6 @@ import java.lang.annotation.Target;
 
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
-@Import(ZKEEConfig.class)
+@Import(ZKEEConfig.class)  /*ZK EE includes CE config*/
 public @interface ZKEEApplication {
 }
